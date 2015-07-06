266e3000d7efeb572c786824319354bd741ec178
amtwo <amtwo@users.noreply.github.com>
2015-07-06 19:49:49 -0400

    Update dbo.adm_hadr_db_role.sql

--- dbo.adm_hadr_db_role.sql
+++ dbo.adm_hadr_db_role.sql
@@ -5,6 +5,27 @@ CREATE FUNCTION dbo.adm_hadr_db_role
 RETURNS nvarchar(60)
 AS
 BEGIN
+/*************************************************************************************************
+AUTHOR: Andy Mallon
+CREATED: 20150706
+    If an Availability Group name is passed into @Name, returns PRIMARY or SECONDARY, depending
+    on the current role of that AG on this server.
+    If a database name is passed as @Name and that DB is part of an AG, returns 
+    PRIMARY or SECONDARY, depending on the current role of that AG on this server.
+    If a database name is passed as @Name and that DB is NOT part of an AG, returns the 
+    database_state_desc from sys.databases
+PARAMETERS
+* @Name - sysname - The name of a Database or Availability Group
+**************************************************************************************************
+MODIFICATIONS:
+    YYYYMMDDD - Initials - Description of changes
+**************************************************************************************************
+    This code is free to download and use for personal, educational, and internal 
+    corporate purposes, provided that this header is preserved. Redistribution or sale, 
+    in whole or in part, is prohibited without the author's express written consent.
+    
+    Copyright 2015 Andy Mallon, www.impatientdba.com
+*************************************************************************************************/
 	DECLARE @Role nvarchar(60);
 	DECLARE @Sql nvarchar(max);
 
@@ -13,16 +34,16 @@ BEGIN
 	IF CAST(LEFT(CAST(SERVERPROPERTY('ProductVersion') AS nvarchar(100)),2) as int) >= 11
 		WITH hadr_role 
 		AS (
-			SELECT d.name  COLLATE SQL_Latin1_General_CP1_CI_AS AS Name, 
-				d.state_desc  COLLATE SQL_Latin1_General_CP1_CI_AS AS StateDesc, 
-				rs.role_desc  COLLATE SQL_Latin1_General_CP1_CI_AS AS RoleDesc
+			SELECT d.name  COLLATE database_default AS Name, 
+				d.state_desc  COLLATE database_default AS StateDesc, 
+				rs.role_desc  COLLATE database_default AS RoleDesc
 			FROM sys.databases d
 			LEFT JOIN sys.dm_hadr_availability_replica_states rs
 				ON rs.replica_id = d.replica_id AND rs.is_local = 1
 			UNION ALL
-			SELECT ag.name COLLATE SQL_Latin1_General_CP1_CI_AS, 
-				rs.operational_state_desc COLLATE SQL_Latin1_General_CP1_CI_AS, 
-				rs.role_desc  COLLATE SQL_Latin1_General_CP1_CI_AS
+			SELECT ag.name COLLATE database_default, 
+				rs.operational_state_desc COLLATE database_default, 
+				rs.role_desc  COLLATE database_default
 			FROM sys.availability_groups ag
 			LEFT JOIN sys.dm_hadr_availability_replica_states rs
 				ON rs.group_id = ag.group_id AND rs.is_local = 1
